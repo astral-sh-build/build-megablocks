910853d5fded0987bf1d928f384f541f1172860b
Charlie Marsh <charlie.r.marsh@gmail.com>
2025-05-23 14:25:58 -0400

    Force CXX11

--- prepare_for_build.sh
+++ prepare_for_build.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
-# Script to prepare the build environment for DeepSpeed.
+# Script to prepare the build environment for Megablocks.
 #
 # Example usage:
-#   ./prepare_for_build.sh v0.16.7
+#   ./prepare_for_build.sh v0.9.0
 
 set -euxo pipefail
 
 export ROOT=`pwd`
 
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 <deepspeed_version>"
-    echo "Example: $0 v0.16.7"
+    echo "Usage: $0 <megablocks_version>"
+    echo "Example: $0 v0.9.0"
     exit 1
 fi
 
-DEEPSPEED_VERSION=$1
+MEGABLOCKS_VERSION=$1
 
-# # Ensure that the DeepSped version is supported.
-# if [ ! -d "${ROOT}/build_scripts/patches/${DEEPSPEED_VERSION}" ]; then
-#     echo "Error: patches/${DEEPSPEED_VERSION} directory does not exist"
-#     exit 1
-# fi
+# Ensure that the Megablocks version is supported.
+if [ ! -d "${ROOT}/build_scripts/patches/${MEGABLOCKS_VERSION}" ]; then
+    echo "Error: patches/${MEGABLOCKS_VERSION} directory does not exist"
+    exit 1
+fi
 
-python -c "import torch; print(torch._C._GLIBCXX_USE_CXX11_ABI)"
-
-# # Apply patches.
-# for patch in "${ROOT}/build_scripts/patches/${DEEPSPEED_VERSION}"/*.patch; do
-#     patch -p1 -d ${ROOT} -i ${patch}
-# done
+# Apply patches.
+for patch in "${ROOT}/build_scripts/patches/${MEGABLOCKS_VERSION}"/*.patch; do
+    patch -p1 -d ${ROOT} -i ${patch}
+done
